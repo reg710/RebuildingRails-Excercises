614cc0ed245090e014f3738f45cc7d9247dba82e
rcon <reg710@gmail.com>
2021-11-15 19:38:57 -0500

    Exercise 1 of chpat 2 , which shows hash that application gets from Rack.

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,6 +1,8 @@
 class QuotesController < Rulers::Controller
     def a_quote
         "there is nothing either good or bad " +
-        "but thinking makes it so."
+        "but thinking makes it so." +
+        # This shows the hash that is everything the application receives from rack
+        "\n<pre>\n#{env}\n</pre>"
     end
 end
