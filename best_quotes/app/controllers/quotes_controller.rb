dbccc5cc1ecd036979cb10bfb1724b809a98345c
rcon <reg710@gmail.com>
2021-11-29 16:46:11 -0500

    Finished Chapter 4 and exercises

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -2,10 +2,9 @@
 
 class QuotesController < Rulers::Controller
     def a_quote
-        "there is nothing either good or bad " +
-        "but thinking makes it so." +
         # This shows the hash that is everything the application receives from rack
-        "\n<pre>\n#{env}\n</pre>"
+        # "\n<pre>\n#{env}\n</pre>"
+        render :a_quote, :noun => :winking
     end
 
     def exception
